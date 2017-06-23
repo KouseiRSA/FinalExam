2e5f5a9ceaed9bbaf9971e168b28f4263d7699f4
Kousei <rainbowstaratelier@gmail.com>
2017-06-23 11:44:48 +0800

    10

---
+++ New Unity Project 1/Assets/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveVertical,moveHorizontal);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ( "block"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 8)
+		{
+			winText.text = "You Win!"; 
+			Debug.Log ("Win");		
+
+
+		}
+	}
+}
